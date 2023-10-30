5005f87a4dbddeb7bea11b47b1a54f6035f4436d
Marcos Piemontez <marcos.piemontez1@gmail.com>
2023-10-30 00:46:39 -0400

    chore: Modularize global sidebar CSS styles.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import "./assets/main.css";
+import "@/assets/styles/main.css";
+import '@/assets/styles/sidebar.css'; 
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import App from "@/App.vue";
+import router from "@/router/index";
 
 const app = createApp(App);
 app.use(router);
